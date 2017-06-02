7ad2cce158568564e6641c21f6b39859a8a0dc02
Bruno Vitor Gomes Prado <BPIPO@bndes.net>
2017-06-02 18:08:22 -0300

    Criada entidade Perfil

---
+++ SocialNetwork.web/Models/Perfil/Perfil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.web.Models
+{
+    public class Perfil
+    {
+        public int PerfilId { get; set; }
+        public int UserID { get; set; }
+        public string NomeExibicao { get; set; }
+        public string FotoPerfil { get; set; }
+    }
+}
