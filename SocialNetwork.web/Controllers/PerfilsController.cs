82d778085cf48ddc2c8f8f27f8f3206c9d0fd7f4
bpipo <bpipo@D-00893986>
2017-06-08 14:34:34 -0300

    Excluido Cheklict.txt

--- SocialNetwork.web/Controllers/PerfilsController.cs
+++ SocialNetwork.web/Controllers/PerfilsController.cs
@@ -77,8 +77,6 @@ namespace SocialNetwork.web.Controllers
         }
 
         // POST: Perfils/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "id,UserID,NomeExibicao,FotoPerfil")] Perfil perfil)
