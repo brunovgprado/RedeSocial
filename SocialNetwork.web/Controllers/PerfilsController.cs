bb47c3752d2d012290aa9304b45e508e49a1d64e
bpipo <bpipo@D-00893986>
2017-06-09 11:32:24 -0300

    Corrigido bug na Action CheckIn

--- SocialNetwork.web/Controllers/PerfilsController.cs
+++ SocialNetwork.web/Controllers/PerfilsController.cs
@@ -11,14 +11,14 @@ namespace SocialNetwork.web.Controllers
         private string IdUsuario;
 
         public PerfilsController()
-        {
-            IdUsuario = Session["UserId"].ToString();
+        {     
             servico = new PerfilServico(new PerfisEntity());
         }
 
         // Action responsável por verificar se o usuário já possui perfil
         public ActionResult CheckIn()
-        {      
+        {
+            IdUsuario = Session["UserId"].ToString();   
             var perfil = servico.RetornaPerfilUsuario(IdUsuario);
 
             if (perfil != null)
