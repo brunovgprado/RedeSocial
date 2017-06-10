57e212b2bab2985540e08116c6acce86d44d357f
bpipo <bpipo@D-00893986>
2017-06-10 00:45:32 -0300

    Adicionado projeto RedeSocialWeb com atenticacao

---
+++ RedeSocialWeb/Controllers/PerfilsController.cs
@@ -0,0 +1,129 @@
+﻿using System.Web.Mvc;
+using Dados;
+using Negocio.Dominio;
+using Servico;
+
+namespace RedeSocialWeb.Controllers
+{
+    public class PerfilsController : Controller
+    {
+        private PerfilServico servico;
+        private string IdUsuario;
+
+        public PerfilsController()
+        {     
+            servico = new PerfilServico(new PerfisEntity());
+        }
+
+        // Action responsável por verificar se o usuário já possui perfil
+        public ActionResult CheckIn()
+        {
+            IdUsuario = Session["UserId"].ToString();   
+            var perfil = servico.RetornaPerfilUsuario(IdUsuario);
+
+            if (perfil != null)
+            {
+                return RedirectToAction("Details",  new { Id = perfil.id });
+            }
+            return RedirectToAction("Create");
+        }
+
+        // Action que registra o Seguir para perfil
+        public ActionResult Seguir(int id)
+        {
+            return RedirectToAction("SeguirPerfil","Seguirs", new { Id = id });
+        }
+
+        // GET: Perfils
+        public ActionResult Index()
+        {
+            var lista = servico.RetornaPerfis();
+            return View(lista);
+        }
+
+        // GET: Perfils/Details/5
+        public ActionResult Details(int id)
+        {
+            Perfil perfil = servico.RetornaPerfilUnico(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // GET: Perfils/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Perfils/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,UserID,NomeExibicao,FotoPerfil")] Perfil perfil)
+        {
+            perfil.UserID = Session["UserId"].ToString();
+            if (ModelState.IsValid)
+            {
+                servico.CriaPerfil(perfil);
+                return RedirectToAction("Details", new { id = perfil.id});
+            }
+
+            return View(perfil);
+        }
+
+        // GET: Perfils/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Perfil perfil = servico.RetornaPerfilUnico(id);
+            if (perfil.UserID == Session["UserId"].ToString())
+            {      
+                return View(perfil);
+            }
+            return RedirectToAction("Index", "Gerenciador");
+        }
+
+        // POST: Perfils/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,UserID,NomeExibicao,FotoPerfil")] Perfil perfil)
+        {
+            if (ModelState.IsValid)
+            {
+                servico.EditaPerfil(perfil);
+                return RedirectToAction("Details", new { id = perfil.id });
+            }
+            return View(perfil);
+        }
+
+        // GET: Perfils/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Perfil perfil = servico.RetornaPerfilUnico(id);
+            if (perfil == null)
+            {
+                return HttpNotFound();
+            }
+            return View(perfil);
+        }
+
+        // Action que apaga um perfil
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public void DeleteConfirmed(int id)
+        {
+            Perfil perfil = servico.RetornaPerfilUnico(id);
+            servico.ApagaPerfil(perfil);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+               servico.dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
