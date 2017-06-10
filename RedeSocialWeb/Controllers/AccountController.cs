9c0b09f448bfe36088d50f61193380beb45865ed
bpipo <bpipo@D-00893986>
2017-06-10 13:04:02 -0300

    Configurado e testado Password recovery

--- RedeSocialWeb/Controllers/AccountController.cs
+++ RedeSocialWeb/Controllers/AccountController.cs
@@ -119,10 +119,6 @@ namespace RedeSocialWeb.Controllers
                 return View(model);
             }
 
-            // The following code protects for brute force attacks against the two factor codes. 
-            // If a user enters incorrect codes for a specified amount of time then the user account 
-            // will be locked out for a specified amount of time. 
-            // You can configure the account lockout settings in IdentityConfig
             var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
@@ -159,20 +155,11 @@ namespace RedeSocialWeb.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
+                    
+                    //Gerando token e enviando e-mail de confirmação
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    /*/Gerando token e enviando e-mail de confirmação
-                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    code = HttpUtility.UrlEncode(code);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id,
-                       "Confirme seu e-mail ", "Por favor, confirme seu e-mail clicando <a href=\""
-                       + model.CallbackUrl + $"?userId={user.Id}&code={code}" + "\">aqui</a>");*/
+                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Por favor, confirme seu e-mail clicando <a href=\"" + callbackUrl + "\">Aqui</a>");  
 
                     return RedirectToAction("Index", "Home");
                 }
@@ -222,10 +209,10 @@ namespace RedeSocialWeb.Controllers
 
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
+                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
