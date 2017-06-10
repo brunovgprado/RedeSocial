3754e021296fdddf183199d3d6979b1d2dd8349d
bpipo <bpipo@D-00893986>
2017-06-10 12:20:55 -0300

    configurando envio de emails

--- RedeSocialWeb/Controllers/AccountController.cs
+++ RedeSocialWeb/Controllers/AccountController.cs
@@ -159,12 +159,20 @@ namespace RedeSocialWeb.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    //Gerando token e enviando e-mail de confirmação
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    code = HttpUtility.UrlEncode(code);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    await UserManager.SendEmailAsync(user.Id,
+                       "Confirme seu e-mail ", "Por favor, confirme seu e-mail clicando <a href=\""
+                       + model.CallbackUrl + $"?userId={user.Id}&code={code}" + "\">aqui</a>");
 
                     return RedirectToAction("Index", "Home");
                 }
--- RedeSocialWeb/Models/AccountViewModels.cs
+++ RedeSocialWeb/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace RedeSocialWeb.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public string CallbackUrl { get; set; }
     }
 
     public class ResetPasswordViewModel
