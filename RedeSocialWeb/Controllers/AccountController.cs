20ab852f8a062c32f1ee37a13479822f3bd42eda
bpipo <bpipo@D-00893986>
2017-06-10 02:24:48 -0300

    configurado context e desmembrado SeguirsController para DDD

--- RedeSocialWeb/Controllers/AccountController.cs
+++ RedeSocialWeb/Controllers/AccountController.cs
@@ -73,17 +73,20 @@ namespace RedeSocialWeb.Controllers
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    var user = UserManager.FindByEmail(model.Email);
+                    Session["UserId"] = user.Id; 
+                    return RedirectToAction("CheckIn", "Perfils");
+
                 case SignInStatus.LockedOut:
                     return View("Lockout");
+
                 case SignInStatus.RequiresVerification:
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
