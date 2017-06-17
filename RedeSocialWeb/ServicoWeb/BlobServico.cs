a07bd8c548f4d9b841727fdb8f6515b06afda29f
bpipo <bpipo@D-00893986>
2017-06-16 22:11:17 -0300

    Adicionado pacote ClouConfigurationManager

---
+++ RedeSocialWeb/ServicoWeb/BlobServico.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+using System.Web;
+using Microsoft.Azure;
+
+namespace RedeSocialWeb.ServicoWeb
+{
+    public class BlobServico
+    {
+        public async Task<string> UploadImageAsync(HttpPostedFileBase imageToUpload)
+        {
+            string imageFullPath = null;
+            if (imageToUpload == null || imageToUpload.ContentLength == 0)
+            {
+                return null;
+            }
+            try
+            {
+                CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("fotoperfil");
+
+                if (await cloudBlobContainer.CreateIfNotExistsAsync())
+                {
+                    await cloudBlobContainer.SetPermissionsAsync(
+                        new BlobContainerPermissions
+                        {
+                            PublicAccess = BlobContainerPublicAccessType.Blob
+                        }
+                        );
+                }
+                string imageName = Guid.NewGuid().ToString() + "-" + Path.GetExtension(imageToUpload.FileName);
+
+                CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(imageName);
+                cloudBlockBlob.Properties.ContentType = imageToUpload.ContentType;
+                await cloudBlockBlob.UploadFromStreamAsync(imageToUpload.InputStream);
+
+                imageFullPath = cloudBlockBlob.Uri.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return imageFullPath;
+        }
+    }
+}
